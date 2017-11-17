7b870cb3b0eaf54dde3b180e41675d119b0016e3
tareq-rar <tareq.rar@gmail.com>
2017-11-17 22:25:21 +0200

    17/11

--- src/container/book-list.js
+++ src/container/book-list.js
@@ -1,7 +1,8 @@
 /*** Created by tareq on 28/10/17.*/
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-export default class BookList extends Component {
+
+class BookList extends Component {
     renderList(){
         return this.props.books.map((book) => {
             return(
@@ -18,5 +19,12 @@ export default class BookList extends Component {
             </ul>
         )
     }
-
 }
+
+function mapStateToProps(state){
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
